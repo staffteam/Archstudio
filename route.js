cfeaf4f7be375167c065ddefd28ca6a60f9b61e0
LonelyHobo <1144646550@qq.com>
2019-12-08 20:48:23 +0800

    1

--- route.js
+++ route.js
@@ -35,12 +35,37 @@ let route = [
         page:'/chair.html'
     },
     {
-        title:'新闻详情',
-        route:'/newsDetails',
-        page:'/newsDetails.html'
+        title:'出版',
+        route:'/publish',
+        page:'/publish.html'
     },
     {
-        title:'加盟我们',
+        title:'韩文强',
+        route:'/hwq',
+        page:'/hwq.html'
+    },
+    {
+        title:'建筑营',
+        route:'/about',
+        page:'/about.html'
+    },
+    {
+        title:'荣誉',
+        route:'/honor',
+        page:'/honor.html'
+    },
+    {
+        title:'荣誉详情',
+        route:'/honorDetail',
+        page:'/honorDetail.html'
+    },
+    {
+        title:'联系',
+        route:'/contact',
+        page:'/contact.html'
+    },
+    {
+        title:'加入',
         route:'/join',
         page:'/join.html'
     }
