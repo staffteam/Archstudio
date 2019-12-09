04db606bfb7fd53a741917f09a9dab138fa3c4b2
LonelyHobo <1144646550@qq.com>
2019-12-09 23:34:28 +0800

    12

--- route.js
+++ route.js
@@ -1,73 +1,77 @@
-let route = [
-    {
-        title:'首页',
-        route:'/',
-        page:'/index.html'
+let route = [{
+        title: '首页',
+        route: '/',
+        page: '/index.html'
     },
     {
-        title:'首页',
-        route:'/index',
-        page:'/index.html'
+        title: '首页',
+        route: '/index',
+        page: '/index.html'
     },
     {
-        title:'作品',
-        route:'/product',
-        page:'/product.html'
+        title: '作品',
+        route: '/product',
+        page: '/product.html'
     },
     {
-        title:'研究',
-        route:'/study',
-        page:'/study.html'
+        title: '研究',
+        route: '/study',
+        page: '/study.html'
     },
     {
-        title:'作品列表',
-        route:'/productList',
-        page:'/productList.html'
+        title: '作品列表',
+        route: '/productList',
+        page: '/productList.html'
     },
     {
-        title:'作品详情',
-        route:'/productDetail',
-        page:'/productDetail.html'
+        title: '作品详情',
+        route: '/productDetail',
+        page: '/productDetail.html'
     },
     {
-        title:'讲座',
-        route:'/chair',
-        page:'/chair.html'
+        title: '讲座',
+        route: '/chair',
+        page: '/chair.html'
     },
     {
-        title:'出版',
-        route:'/publish',
-        page:'/publish.html'
+        title: '出版',
+        route: '/publish',
+        page: '/publish.html'
     },
     {
-        title:'韩文强',
-        route:'/hwq',
-        page:'/hwq.html'
+        title: '韩文强',
+        route: '/hwq',
+        page: '/hwq.html'
     },
     {
-        title:'建筑营',
-        route:'/about',
-        page:'/about.html'
+        title: '建筑营',
+        route: '/about',
+        page: '/about.html'
     },
     {
-        title:'荣誉',
-        route:'/honor',
-        page:'/honor.html'
+        title: '荣誉',
+        route: '/honor',
+        page: '/honor.html'
     },
     {
-        title:'荣誉详情',
-        route:'/honorDetail',
-        page:'/honorDetail.html'
+        title: '荣誉详情',
+        route: '/honorDetail',
+        page: '/honorDetail.html'
     },
     {
-        title:'联系',
-        route:'/contact',
-        page:'/contact.html'
+        title: '联系',
+        route: '/contact',
+        page: '/contact.html'
     },
     {
-        title:'加入',
-        route:'/join',
-        page:'/join.html'
+        title: '加入',
+        route: '/join',
+        page: '/join.html'
+    },
+    {
+        title: '展览',
+        route: '/exhibition',
+        page: '/exhibition.html'
     }
 ];
 module.exports = route;
