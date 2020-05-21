1d044e3c13f4eb0a1b69d61b29b9b0e808f494b9
LonelyHobo <1144646550@qq.com>
2020-05-21 20:27:40 +0800

    1111

--- route.js
+++ route.js
@@ -24,9 +24,59 @@ let route = [{
         page: '/productList.html'
     },
     {
+        title: '作品列表',
+        route: '/productList01',
+        page: '/productList01.html'
+    },
+    {
+        title: '作品列表',
+        route: '/productList02',
+        page: '/productList02.html'
+    },
+    {
+        title: '作品列表',
+        route: '/productList03',
+        page: '/productList03.html'
+    },
+    {
         title: '作品详情',
         route: '/productDetail',
         page: '/productDetail.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailA',
+        page: '/productDetailA.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailB',
+        page: '/productDetailB.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailC',
+        page: '/productDetailC.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailD',
+        page: '/productDetailD.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailE',
+        page: '/productDetailE.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailF',
+        page: '/productDetailF.html'
+    },
+    {
+        title: '作品详情',
+        route: '/productDetailG',
+        page: '/productDetailG.html'
     },
     {
         title: '讲座',
@@ -82,6 +132,21 @@ let route = [{
         title: '展览详情',
         route: '/exhibitionDetail',
         page: '/exhibitionDetail.html'
+    },
+    {
+        title: '关于建筑营',
+        route: '/jzy',
+        page: '/jzy.html'
+    },
+    {
+        title: '价值观',
+        route: '/value',
+        page: '/value.html'
+    },
+    {
+        title: '服务范围',
+        route: '/serviceArea',
+        page: '/serviceArea.html'
     }
 ];
 module.exports = route;
